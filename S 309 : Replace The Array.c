f71a3240ef1cb746698fafee3b6e9098ade71bd4
Gaurang Gujrati <72229250+Gaurang105@users.noreply.github.com>
2023-03-06 14:41:23 +0530

    Update S 309 : Replace The Array.c
    Previous code is not working so I'm making a PR with this new python code

--- S 309 : Replace The Array.c
+++ S 309 : Replace The Array.c
@@ -1,29 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+n = int(input())
+a = list(map(int, input().split()))
 
-int main() 
-{
-    int n;
-    scanf("%d",&n);
-    
-    int arr[n], i;
-    for(i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-    
-    int max = arr[n-1];
-    arr[n-1] = 0;
-    
-    for(i=n-2;i>=0;i--)
-    {
-        int temp = arr[i];
-        arr[i] = max;
-        if(max < temp)
-            max = temp;
-    }
-    
-    for(i=0;i<n;i++)
-        printf("%d ",arr[i]);
-    return 0;
-}
+max_right = 0
+for i in range(n-1, -1, -1):
+    temp = a[i]
+    a[i] = max_right
+    max_right = max(max_right, temp)
+a[n-1] = 0
+
+print(*a)
